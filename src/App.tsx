74e937b8dc167ea04b533dcbb523f8f6ff7e1f93
Sam <sam.mjalaoui@gmail.com>
2024-01-31 12:45:45 +0100

    add app main theme

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,40 @@
 
-function App() {
+import { Container, Paper, Typography } from "@mui/material";
+import { ThemeProvider, createTheme } from "@mui/material/styles";
 
+const theme = createTheme({
+  typography: {
+    fontFamily: "Omnes, sans-serif",
+  },
+  palette: {
+    primary: {
+      main: "#f2f3f5", // grey
+    },
+    secondary: {
+      main: "#009de0", // blue
+    },
+  },
+});
+
+function App() {
   return (
-    <>
-      <h1>Delivery Fee Calculator</h1>
-    </>
-  )
+    <ThemeProvider theme={theme}>
+      <Container maxWidth="sm">
+        <Paper
+          elevation={6}
+          sx={{
+            padding: "20px",
+            textAlign: "center",
+            backgroundColor: "primary.main",
+          }}
+        >
+          <Typography variant="h4" sx={{ fontWeight: "bold" }} gutterBottom>
+            Delivery Fee Calculator
+          </Typography>
+        </Paper>
+      </Container>
+    </ThemeProvider>
+  );
 }
 
-export default App
+export default App;
