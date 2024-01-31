b82c20e9d4440148e5857e25c2ed374ea267585a
Sam <sam.mjalaoui@gmail.com>
2024-01-31 11:58:54 +0100

    install jest

---
+++ jest.config.ts
@@ -0,0 +1,9 @@
+export default {
+  preset: 'ts-jest',
+  testEnvironment: 'jsdom',
+  setupFilesAfterEnv: [ '<rootDir>/jest-setup.ts'],
+  moduleNameMapper: {
+    '\\.(css|less|scss|sass)$': 'identity-obj-proxy',
+  },
+};
+  
